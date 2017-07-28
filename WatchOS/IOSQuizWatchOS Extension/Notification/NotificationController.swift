633b3aeea0c9a984604b926666a992e66e300022
Andrew Galushka <andreygshadow@gmail.com>
2017-07-28 14:13:05 +0300

    All files grouped by folder.

---
+++ WatchOS/IOSQuizWatchOS Extension/Notification/NotificationController.swift
@@ -0,0 +1,27 @@
+//
+//  NotificationController.swift
+//  IOSQuizWatchOS Extension
+//
+//  Created by galushka on 7/3/17.
+//  Copyright © 2017 galushka. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import UserNotifications
+
+
+class NotificationController: WKUserNotificationInterfaceController {
+
+    override init() {
+        super.init()
+    }
+
+    override func willActivate() {
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        super.didDeactivate()
+    }
+}
